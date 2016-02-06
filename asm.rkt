7798e2cd369a1b1cc77ba165dc5ca376f0ea53d3
Heli Wang <wang.heli2013@outlook.com>
2016-02-06 14:28:49 -0500

    bet one

--- asm.rkt
+++ asm.rkt
@@ -255,7 +255,13 @@
     [(list (token 'id '(#\s #\l #\t #\u)) (token 'register d) (token 'comma '(#\,)) (token 'register s) (token 'comma '(#\,)) (token 'register t))
      (output (bitwise-ior (arithmetic-shift 0 26) (arithmetic-shift s 21) (arithmetic-shift t 16) (arithmetic-shift d 11) (arithmetic-shift 43 0)))]
     [(list (token 'id '(#\b #\e #\q)) (token 'register d) (token 'comma '(#\,)) (token 'register s) (token 'comma '(#\,)) (token 'int i))
-     (output (bitwise-ior (arithmetic-shift 4 26) (arithmetic-shift d 21) (arithmetic-shift s 16)  (bitwise-and i #xffff)))]
+     (if (and (>= i -32768)(<= i 32767)) (output (bitwise-ior (arithmetic-shift 4 26) (arithmetic-shift d 21) (arithmetic-shift s 16)  (bitwise-and i #xffff))) (error 'ERROR "exceed 0xffff\n"))]
+    [(list (token 'id '(#\b #\n #\e)) (token 'register d) (token 'comma '(#\,)) (token 'register s) (token 'comma '(#\,)) (token 'int i))
+     (if (and (>= i -32768)(<= i 32767)) (output (bitwise-ior (arithmetic-shift 5 26) (arithmetic-shift d 21) (arithmetic-shift s 16)  (bitwise-and i #xffff))) (error 'ERROR "exceed 0xffff\n"))]
+    [(list (token 'id '(#\b #\e #\q)) (token 'register d) (token 'comma '(#\,)) (token 'register s) (token 'comma '(#\,)) (token 'hexint i))
+     (if (and (>= i 0)(<= i 65535)) (output (bitwise-ior (arithmetic-shift 4 26) (arithmetic-shift d 21) (arithmetic-shift s 16)  (bitwise-and i #xffff))) (error 'ERROR "exceed 0xffff\n"))]
+    [(list (token 'id '(#\b #\n #\e)) (token 'register d) (token 'comma '(#\,)) (token 'register s) (token 'comma '(#\,)) (token 'hexint i))
+     (if (and (>= i 0)(<= i 65535)) (output (bitwise-ior (arithmetic-shift 5 26) (arithmetic-shift d 21) (arithmetic-shift s 16)  (bitwise-and i #xffff))) (error 'ERROR "exceed 0xffff\n"))]
     [else (error 'ERROR "unexpected commend line\n")])])))
 
 
